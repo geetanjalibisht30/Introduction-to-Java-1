06224edb759febfa13fdecec112bd736751fb648
Geetanjali Bisht <geetanjali.bisht@tothenew.com>
2022-09-27 12:14:32 +0530

    hii

---
+++ IdeaProjects/Practice1/src/Overloading.java
@@ -0,0 +1,17 @@
+class Adder{
+    static  int add(int a, int b){
+        return (a+b);
+    }
+    static int add(int a, int b , int c){
+        return (a+b+c);
+    }
+}
+
+
+public class Overloading {
+
+    public static void main (String[] args){
+    System.out.println(Adder.add(2,3));
+    System.out.println(Adder.add(2,6));
+    }
+}
