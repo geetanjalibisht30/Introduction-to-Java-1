a89c158952ff62f17f290eb32878e410d28376cb
Geetanjali Bisht <geetanjali.bisht@tothenew.com>
2022-10-03 09:48:32 +0530

    Assignment5

--- IdeaProjects/Java9-17/src/Ans2.java
+++ IdeaProjects/Java9-17/src/Ans2.java
@@ -1,15 +1,16 @@
+import java.net.SocketOption;
 import java.util.Arrays;
 import java.util.*;
 public class Ans2 {
     public static void main(String[] args){
-        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8);
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
         System.out.println("----------- takeWhile---------------");
 
         list.stream().takeWhile(e->e<5).forEach(System.out::println);
 
         System.out.println("----------- dropWhile---------------");
-        list.stream().dropWhile(e->e<5).forEach(System.out::println );
+        list.stream().dropWhile(e->e<6).forEach(System.out::println );
 
     }
     }
-}
+
---
+++ IdeaProjects/Java9-17/src/Ans7.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Ans7 {
+
+    public static void main(String[] args) {
+        System.out.println(List.of(1,2,3,4,5,6,7,8));
+        System.out.println(Set.of(9,10,11,12,13,14,15,16,17,18));
+        System.out.println(Map.of(1,"one",2,"two",3,"three"));
+
+    }
+}
+
