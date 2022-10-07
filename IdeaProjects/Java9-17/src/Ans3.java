dbec31a8b35b06c7830364f6246bf537474dcabf
geetanjalibisht30 <114075734+geetanjalibisht30@users.noreply.github.com>
2022-10-07 09:56:51 +0530

    Delete IdeaProjects/Java9-17/src directory

--- IdeaProjects/Java9-17/src/Ans3.java
+++
@@ -1,8 +0,0 @@
-import java.util.stream.IntStream;
-
-public class Ans3 {
-
-    public static void main(String[] args) {
-        IntStream.rangeClosed(1,5).forEach(System.out::println);
-    }
-}
--- IdeaProjects/Java9-17/src/Ans4.java
+++
@@ -1,7 +0,0 @@
-import java.util.stream.IntStream;
-
-public class Ans4 {
-    public static void main(String[] args) {
-        IntStream.iterate(0,e->e<30,e->e+2).forEach(System.out::println);
-    }
-}
